3a233f8d92fc4fb87c011843ffc8367e897db18b
Mathias Fussenegger <f.mathias@zignar.net>
2015-11-30 22:35:48 +0100

    extend setup.py

--- setup.py
+++ setup.py
@@ -3,15 +3,32 @@
 
 from setuptools import setup
 
+try:
+    with open('README.rst', 'r', encoding='utf-8') as f:
+        readme = f.read()
+except IOError:
+    readme = ''
+
 
 setup(
     name='cr8',
-    version='0.1',
+    author='Mathias Fußenegger',
+    author_email='pip@zignar.net',
+    url='https://github.com/mfussenegger/crate-devtools',
+    long_description=readme,
     entry_points={
         'console_scripts': [
             'cr8 = cr8.main:main',
         ]
     },
     packages=['cr8'],
-    install_requires=['crate', 'argh', 'requests', 'tqdm', 'fake-factory']
+    install_requires=['crate', 'argh', 'requests', 'tqdm', 'fake-factory'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+    use_scm_version=True,
+    setup_requires=['setuptools_scm']
 )
