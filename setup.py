35efd40609b1049bed9b485b1ee257699834973b
Mathias Fussenegger <f.mathias@zignar.net>
2017-01-06 22:09:11 +0100

    Add py36 classifier

--- setup.py
+++ setup.py
@@ -39,6 +39,7 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
     ],
     use_scm_version=True,
     setup_requires=['setuptools_scm']
