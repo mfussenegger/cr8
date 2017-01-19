f67150f00aa6e869d3a42889ee0f821fa5932c68
Mathias Fussenegger <f.mathias@zignar.net>
2017-01-19 23:51:05 +0100

    fixup! WIP: calibrate

--- cr8/calibrate.py
+++ cr8/calibrate.py
@@ -1,17 +1,15 @@
 import itertools
+import statistics
 from functools import partial
 
-from cr8.run_crate import CrateNode, get_crate
 from cr8.engine import Runner
 
 
-def _get_mean(crate_dir, statement, warmup, iterations):
-    with CrateNode(crate_dir=crate_dir) as n:
-        n.start()
-        with Runner(n.http_url, concurrency=1) as r:
-            r.warmup(statement, warmup)
-            stats = r.run(statement, iterations)
-            return stats.stats.get()['mean']
+def _get_median(hosts, statement, warmup, iterations):
+    with Runner(hosts, concurrency=1) as r:
+        r.warmup(statement, warmup)
+        stats = r.run(statement, iterations)
+        return stats.stats.get()['median']
 
 
 def _pairwise(iterable):
@@ -25,30 +23,33 @@ def _perc_diff(a, b):
 
 
 def _within_perc(values, perc):
-    diffs = (_perc_diff(a, b) for a, b in _pairwise(values))
+    mean = statistics.harmonic_mean(values)
+    print('Values: ', values)
+    print('Harmonic mean: ', mean)
+    diffs = (_perc_diff(mean, i) for i in values)
     diffs = list(diffs)
-    print(diffs)
+    print([f'{d:.3f}' for d in diffs])
     return all(d < perc for d in diffs)
 
 
 def calibrate(statement,
               version='latest-stable',
+              hosts='localhost:4200',
               start_warmup=1,
               start_iterations=10,
               match_size=10,
               within_perc=1):
-    crate_dir = get_crate(version)
     warmup = start_warmup
     iterations = start_iterations
-    means = []
-    _run = partial(_get_mean, crate_dir, statement)
+    measurements = []
+    _run = partial(_get_median, hosts, statement)
     for i in range(match_size):
-        means.append(_run(warmup, iterations))
-    while not _within_perc(means, perc=within_perc):
+        measurements.append(_run(warmup, iterations))
+    while not _within_perc(measurements, perc=within_perc):
         warmup *= 2
         iterations *= 2
-        means.clear()
+        measurements.clear()
         for i in range(match_size):
-            means.append(_run(warmup, iterations))
+            measurements.append(_run(warmup, iterations))
     print('warmup: ', warmup)
     print('iterations: ', iterations)
