f67af593cf998e47e9d2e02e190a0bae073b690a
Mathias Fussenegger <f.mathias@zignar.net>
2018-07-03 21:45:28 +0200

    Add python 3.7 to setup.py classifiers

--- setup.py
+++ setup.py
@@ -40,6 +40,7 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
     ],
     use_scm_version=True,
     setup_requires=['setuptools_scm']
