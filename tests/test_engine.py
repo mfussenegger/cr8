71f50aeb1936ed0de31e89f9ba4c37f09e02a602
Mathias Fussenegger <f.mathias@zignar.net>
2018-09-13 23:55:38 +0200

    Simplify eval_fail_if

--- tests/test_engine.py
+++ tests/test_engine.py
@@ -14,6 +14,28 @@ class FailIfTest(TestCase):
         with self.assertRaises(FailIf):
             eval_fail_if("'{statement}' == 'select name'", result)
 
+    def test_fail_if_supports_runtime_stats(self):
+        stats = Stats()
+        stats.measure(103.2)
+        stats.measure(205.2)
+        timed_stats = TimedStats(1, 2, stats)
+        result = Result({}, 'select name', timed_stats, 1)
+        with self.assertRaises(FailIf):
+            eval_fail_if("{runtime_stats.max} > 30", result)
+
+    def test_fail_if_supports_concurrency(self):
+        stats = Stats()
+        timed_stats = TimedStats(1, 2, stats)
+        result = Result({}, 'select name', timed_stats, 1)
+        with self.assertRaises(FailIf):
+            eval_fail_if("{concurrency} == 1", result)
+
+    def test_fail_if_supports_bulk_size(self):
+        stats = Stats()
+        timed_stats = TimedStats(1, 2, stats)
+        result = Result({}, 'select name', timed_stats, 1, bulk_size=200)
+        eval_fail_if("{bulk_size} < 200", result)
+
 
 def load_tests(loader, tests, ignore):
     tests.addTests(DocTestSuite(engine))
