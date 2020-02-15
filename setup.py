d98e564f729c84839a31425ef2f58ecc66cc1dc2
Mathias Fussenegger <f.mathias@zignar.net>
2020-02-15 18:04:03 +0100

    Add official python 3.8 support

--- setup.py
+++ setup.py
@@ -41,6 +41,7 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
     ],
     use_scm_version=True,
     setup_requires=['setuptools_scm']
